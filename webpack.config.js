b8e3d206135d78f5215740ff319bb8c27e53a5d3
huanghanzhilian <1319639755@qq.com>
2018-04-14 03:25:24 +0800

    进入页面开发result操作结果页

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,13 @@ var WEBPACK_ENV         = process.env.WEBPACK_ENV || 'dev';
 /**
  * 获取html-webpack-plugin参数的方法
  */
-var getHtmlConfig=function(name){
+var getHtmlConfig=function(name,title){
     return {
         filename: 'view/'+ name +'.html',//放置目标文件的位置
         template:'./src/view/'+ name +'.html',  //模板，html原始模板路径
         inject:'body',//inject: 引入模块的注入位置；取值有true/false/body/head；true | 'head' | 'body' | false  ,注入所有的资源到特定的 template 或者 templateContent 中，如果设置为 true 或者 body，所有的 javascript 资源将被放置到 body 元素的底部，'head' 将放置到 head 元素中
         hash:true,
+        title:title,
         chunks:['common',name]//需要打包的模块
     }
 }
@@ -24,7 +25,8 @@ var config = {
     entry: {
         "common":["./src/page/common/index.js"],
         "index":"./src/page/index/index.js",
-        "login":"./src/page/login/login.js"
+        "login":"./src/page/login/login.js",
+        'result': ['./src/page/result/index.js'],
     },//入口文件
     output: {//输出信息
         path: "./dist",//存放路径 目录名称   // 输出到那个目录下（__dirname当前项目目录）
@@ -67,8 +69,9 @@ var config = {
         //把css单独打包到文件里
         new ExtractTextPlugin("css/[name].css"),
         //html模板的处理
-        new HtmlWebpackPlugin(getHtmlConfig('index')),
-        new HtmlWebpackPlugin(getHtmlConfig('login')),
+        new HtmlWebpackPlugin(getHtmlConfig('index','首页')),
+        new HtmlWebpackPlugin(getHtmlConfig('login','登录页')),
+        new HtmlWebpackPlugin(getHtmlConfig('result','操作结果')),
     ]
 }
 if('dev' === WEBPACK_ENV){
