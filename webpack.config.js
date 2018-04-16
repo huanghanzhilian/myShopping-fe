914c8356a71702c4e89be64f25a49e66dba67a87
huanghanzhilian <1319639755@qq.com>
2018-04-17 01:17:24 +0800

    注册页面

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ var config = {
         "common":["./src/page/common/index.js"],
         "index":"./src/page/index/index.js",
         'user-login'        : ['./src/page/user-login/index.js'],
+        'user-register'     : ['./src/page/user-register/index.js'],
         'result': ['./src/page/result/index.js'],
     },//入口文件
     output: {//输出信息
@@ -71,6 +72,7 @@ var config = {
         //html模板的处理
         new HtmlWebpackPlugin(getHtmlConfig('index','首页')),
         new HtmlWebpackPlugin(getHtmlConfig('user-login','登录页')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
         new HtmlWebpackPlugin(getHtmlConfig('result','操作结果')),
     ]
 }
