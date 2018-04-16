4fafc8f2f16e634a70bddb509179308a56b4d37f
huanghanzhilian <1319639755@qq.com>
2018-04-17 01:59:56 +0800

    个人中心修改信息

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ var config = {
         'user-register'     : ['./src/page/user-register/index.js'],
         'user-pass-reset'   : ['./src/page/user-pass-reset/index.js'],
         'user-center'       : ['./src/page/user-center/index.js'],
+        'user-center-update': ['./src/page/user-center-update/index.js'],
         'result': ['./src/page/result/index.js'],
     },//入口文件
     output: {//输出信息
@@ -77,6 +78,7 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
         new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
         new HtmlWebpackPlugin(getHtmlConfig('result','操作结果')),
     ]
 }
