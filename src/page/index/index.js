3747989d153dd531c30e1e1bb9ff4adc3caf8a1b
huanghanzhilian <1319639755@qq.com>
2018-04-17 02:24:32 +0800

    商品模块,首页静态

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,40 +1,30 @@
-//index.js
-//require('./cats.js');
 /*
-var _mm=require('util/mm.js');
-//ajax使用
-// _mm.request({
-// 	url:'http://m.samured.com/api/mobile/index/videos?pageNum=1&querySize=8',
-// 	success:function(res){
-// 		console.log(res)
-// 	},
-// 	error:function(errMsg){
-// 		console.log(errMsg)
-// 	}
-// });
-//获取服务器地址使用
-// console.log(_mm.getUrlParam('test'));
-// console.log(_mm.getUrlParam('test1'));
-
-//渲染html方法使用
-var html='<div>{{data}}</div>';
-var data={
-	data:'test'
-}
-console.log(_mm.renderHtml(html,data));
-console.log("hellokk")
+* @Author: Rosen
+* @Date:   2017-05-08 15:19:12
+* @Last Modified by:   Rosen
+* @Last Modified time: 2017-05-26 19:36:18
 */
 
-require('page/common/nav-simple/index.js');
+'use strict';
+require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-
+require('util/slider/index.js');
 var navSide         = require('page/common/nav-side/index.js');
+var templateBanner  = require('./banner.string');
+var _mm             = require('util/mm.js');
 
-require('./index.css');
-require('../module.js');
-//alert(1);
-console.log("首页")
-navSide.init({
-            name: 'user-center'
-        });
+$(function() {
+    // 渲染banner的html
+    var bannerHtml  = _mm.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    // 初始化banner
+    var $slider     = $('.banner').unslider({
+        dots: true
+    });
+    // 前一张和后一张操作的事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    });
+});
